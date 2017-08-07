3a5bce26e2a237aa27e8b035f279cbdcc0c8fb8f
oldhill <oldhillcode@gmail.com>
2017-08-06 17:58:00 -0700

    Add macro, function w params, switch to k&r style

--- src/functions/functions.c
+++ src/functions/functions.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 
 
+#define MULTIPLIER 5
+
+
 void say_hello(void);
+int const_multiply(int n);
 
 
-int main() {
-
+int main()
+{
     say_hello();
+    printf("%d\n", const_multiply(5));
 
     return 0;
 }
 
 
-void say_hello(void) {
+void say_hello(void)
+{
     printf("Hello, world\n");
+}
+
+
+int const_multiply(int n) {
+    return MULTIPLIER * n;
 }
